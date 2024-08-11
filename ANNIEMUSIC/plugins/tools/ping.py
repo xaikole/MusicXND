c6a8691da4c1c6cedc7ab486e6a3c31f885dc732
CHALCOGEN645 <166417113+CHALCOGEN645@users.noreply.github.com>
2024-08-11 18:49:22 +0530

    Update ping.py

--- ANNIEMUSIC/plugins/tools/ping.py
+++ ANNIEMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://telegra.ph/file/634addcd0a2828f4e8913.mp4",
+        video="https://telegra.ph/file/9deb97c59b1d281a48c7a.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await JARVIS.ping()
