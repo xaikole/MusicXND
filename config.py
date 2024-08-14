ffa6b684f14a784c257cbd090f276dc22fe84a4a
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-08-14 21:37:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(getenv("OWNER_ID", 6536914119))
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/CHALCOGEN645/PENGUIN-x-MUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/MOON-CODES/MOON-CODESVR")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
